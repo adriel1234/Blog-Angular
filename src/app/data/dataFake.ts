993f23f4feb5fed305a57794fa3b6a05ca4d213d
Adriel Vinicius <adriel1234.1234@gmail.com>
2023-04-26 17:30:59 -0400

    Deixando content page dinamico

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,14 @@
+export const dataFake = [
+    {
+        "id":'1',
+        "title": "Novo Filme Homem de ferro",
+        "description": "Marvel studios anuncia novo filme",
+        "photoCover":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRLXYIBkTb14LZIZaoZqGKn5qgjZqvV2-WxbQ&usqp=CAU"
+    },
+    {
+        "id":'2',
+        "title": "NOVA SERIE ANUNCIADA",
+        "description": "test ets testsetsetst",
+        "photoCover":"https://disneyplusbrasil.com.br/wp-content/uploads/2021/07/Series-Marvel-Disney-Plus-1024x576.jpg"
+    },
+];
